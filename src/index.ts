7ca68f2dd2b3e812ed178867d5bcf4e079552562
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-06-05 15:17:42 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: "https://frontend-ics-nadlan.vercel.app",
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
   })
 );
