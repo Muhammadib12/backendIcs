af43941fb574b65d199de8879bd2c8a584aab847
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-06-05 15:01:22 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-domain.vercel.app",
+      "https://frontend-ics-nadlan.vercel.app/",
     ],
     credentials: true,
   })
